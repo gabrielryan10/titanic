21f7971019675518aea6df5541ba169a8fcc1470
gabrielryan10 <88547969+gabrielryan10@users.noreply.github.com>
2023-06-30 08:15:13 +0800

    Update titanic_app.py

--- titanic_app.py
+++ titanic_app.py
@@ -67,7 +67,7 @@ def titanic_pred(model_titanic,pclass,title_num,sibsp_num,parch_num):
 
 def main():
     st.title("Titanic Survival Score Prediction")
-    st.write('<p style="font-size:16px; color:black;">XGBoost Engine</p>',unsafe_allow_html=True)
+    st.subheader("(XGBoost Engine)")
 
 
     pclass = st.selectbox("Class of Passenger", [1, 2, 3])
